39bef002724daedb2791893af981306fb3185e12
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-04-22 22:44:04 +0530

    fetched get api for book cards.

--- frontend/components/Stock/BookCard.tsx
+++ frontend/components/Stock/BookCard.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import {
   Card,
@@ -16,6 +17,7 @@ interface BookCardProps {
   author: string;
   isbn: string;
   price: number;
+  date: number;
 }
 
 const BookCard = ({
@@ -25,16 +27,19 @@ const BookCard = ({
   price,
   author,
   isbn,
+  date,
 }: BookCardProps) => {
+
+  
   return (
-    <div className="w-[24vw]">
+    <div className="w-[28vw]">
       <Card>
         <CardHeader>
           <CardTitle>{title}</CardTitle>
           <div className="flex flex-col gap-8">
             <div className="flex gap-4 justify-between items-center">
               <CardDescription>{author}</CardDescription>
-              <CardDescription>Publication Year: {isbn}</CardDescription>
+              <CardDescription>Publication Year: {date}</CardDescription>
             </div>
             <CardDescription>ISBN: {isbn}</CardDescription>
           </div>
@@ -42,7 +47,7 @@ const BookCard = ({
         <CardContent>
           <div className="flex justify-between items-center gap-8">
             <span className="flex gap-2 text-zinc-600">
-              <span className="underline"> Stock</span>: {stock}
+              <span className="underline"> Stock</span>: {stock ? stock : 0}
             </span>
             {formatCurrency(price)}
           </div>
--- frontend/components/Stock/Stock.tsx
+++ frontend/components/Stock/Stock.tsx
@@ -1,17 +1,50 @@
+"use client"
 import React from 'react'
 import BookCard from './BookCard'
+import { useEffect, useState } from "react";
+
+type bookDataType = {
+  title: string,
+  desc: string,
+  stock: number,
+  price: number,
+  author: string,
+  ISBN: string,
+  publicationYear: number
+}
 
 const Stock = () => {
+  const [bookData, setBookData] = useState<bookDataType[]>([]);
+
+  useEffect(() => {
+    try{
+      fetch("http://localhost:3001/book")
+      .then((response) => response.json())
+      .then((data) => setBookData(data));
+    }
+    catch(e) {
+      console.log(e)
+    }
+  },[])
+
   return (
-    <div>
-      <BookCard 
-        title="Harry Potter"
-        desc="A Book."
-        stock={10}
-        price={400}
-        author='J.K. Rowling'
-        isbn='1234'
-      />
+    <div className='grid grid-cols-3 gap-y-8 gap-x-2'>
+
+      {
+        bookData.map((book) => (
+          <BookCard 
+          title={book.title}
+          desc={book.desc}
+          date={book.publicationYear}
+          stock={book.stock}
+          price={book.price}
+          author={book.author}
+          isbn={book.ISBN}
+        />
+        ))
+      }
+      
+     
     </div>
   )
 }
