2097c505194a957d6067efd174fc45b78f718ff8
SRIJAN SHARMA <125155693+horiznsky@users.noreply.github.com>
2024-04-22 17:22:10 +0530

    changed schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express =  require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { Book } = require('./models/schema');
 // const databaseurl = "mongodb+srv://ROOM1097:@DBMSdiu@bookstore.mongocluster.cosmos.azure.com/book?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
 const databaseurl = "mongodb+srv://ROOM1097:0DBMSdiu@bookstore.mongocluster.cosmos.azure.com/book?retryWrites=true&w=majority";
 const PORT = 3001;
@@ -20,6 +21,68 @@ mongoose.connect(databaseurl)
 .catch((e)=>{
     console.log(e)
 })
+
+app.post('/book', async (req, res) => {
+    try {
+        const { title, author, category, ISBN, price, stockQuantity, publicationYear, description } = req.body;
+
+        const newBook = new Book({
+            title, 
+            author, 
+            category, 
+            ISBN, 
+            price, 
+            stockQuantity, 
+            publicationYear, 
+            description
+        });
+
+        console.log(newBook)
+        const book = await newBook.save();
+        
+        res.status(201).send(book);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/book', async (req, res) => {
+    try {
+        const { title, author, category, ISBN, price, stockQuantity, publicationYear, description } = req.body;
+
+        const newBook = new Book({
+            title, 
+            author, 
+            category, 
+            ISBN, 
+            price, 
+            stockQuantity, 
+            publicationYear, 
+            description
+        });
+
+        console.log(newBook)
+        const book = await newBook.save();
+        
+        res.status(201).send(book);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/book', async (req, res) => {
+    try {
+        const books = await Book.find();
+        
+        res.status(200).send(books);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 app.get("/", (req,res) => {
     console.log(req);
--- backend/models/schema.js
+++ backend/models/schema.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+const { string, number } = require('zod');
 const Schema = mongoose.Schema;
 
 
 const bookSchema = new Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
-    category: { type: String, required: true },
+    category: { type: String, required: true , lowercase : true},
     ISBN: { type: String, required: true, unique: true },
     price: { type: Number, required: true },
     stockQuantity: { type: Number, default: 0 },
-    publicationDate: { type: Date, required: true },
+    publicationYear: { type: Number, required: true },
     description: { type: String }
 });
 
