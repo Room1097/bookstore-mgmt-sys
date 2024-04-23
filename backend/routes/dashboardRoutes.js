9ca153c162281174ea94765ed296c59dd781edf2
Trijay1709 <trijaypatel1709@gmail.com>
2024-04-23 23:58:43 +0530

    Bug fix

--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -7,16 +7,25 @@ router.get('/revenue', async (req, res) => {
         const books = await Book.find();
         const sales = await Sales.find();
         let revenue = 0;
+        
         for (let i = 0; i < sales.length; i++) {
-            const price = (books.find(value => value._id === sales[i].book)?.price || 0) * sales[i].quantity;
-            revenue += price;
+            const sale = sales[i];
+            const book = books.find(value => value._id.toString() === sale.book.toString());
+            
+            if (book) {
+                const price = book.price * sale.quantity;
+                // console.log(price);
+                revenue += price;
+            }
         }
+        
         res.status(200).json({ revenue: revenue });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
     }
 });
+
 router.get('/stock', async (req, res) => {
     try {
         const books = await Book.find();
